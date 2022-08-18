e28f7523f90936173763e242365bf0749ad9b250
Juan1733 <juan.araque@correo.unimet.edu.ve>
2022-08-18 04:15:45 +0000

    extra 1 added (button)

--- src/app.js
+++ src/app.js
@@ -31,11 +31,7 @@ function generateNumber() {
   return number;
 }
 
-window.onload = function() {
-  const top = document.querySelector(".card-top");
-  const cardNumber = document.querySelector(".number");
-  const bottom = document.querySelector(".card-bottom");
-
+function generateCard(top, cardNumber, bottom) {
   let number = generateNumber();
   let symbol = generateSymbol();
 
@@ -43,9 +39,26 @@ window.onload = function() {
     top.classList.add("red");
     bottom.classList.add("red");
     cardNumber.classList.add("red");
+  } else {
+    top.classList.remove("red");
+    bottom.classList.remove("red");
+    cardNumber.classList.remove("red");
   }
 
   top.innerHTML = symbol;
   cardNumber.innerHTML = number;
   bottom.innerHTML = symbol;
+}
+
+window.onload = function() {
+  const top = document.querySelector(".card-top");
+  const cardNumber = document.querySelector(".number");
+  const bottom = document.querySelector(".card-bottom");
+  const button = document.querySelector(".btn");
+
+  generateCard(top, cardNumber, bottom);
+
+  button.addEventListener("click", function() {
+    generateCard(top, cardNumber, bottom);
+  });
 };
