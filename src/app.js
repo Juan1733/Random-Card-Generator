0b5a7dd794b4e4bb8947324221996faa35ef4d75
Juan1733 <juan.araque@correo.unimet.edu.ve>
2022-08-18 03:45:27 +0000

    ready

--- src/app.js
+++ src/app.js
@@ -5,7 +5,47 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generateSymbol() {
+  let symbols = ["♦️", "♥️", "♠", "♣"];
+  let symbol = symbols[Math.floor(Math.random() * symbols.length)];
+  return symbol;
+}
+
+function generateNumber() {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let number = numbers[Math.floor(Math.random() * numbers.length)];
+  return number;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const top = document.querySelector(".card-top");
+  const cardNumber = document.querySelector(".number");
+  const bottom = document.querySelector(".card-bottom");
+
+  let number = generateNumber();
+  let symbol = generateSymbol();
+
+  if (symbol == "♦️" || symbol == "♥️") {
+    top.classList.add("red");
+    bottom.classList.add("red");
+    cardNumber.classList.add("red");
+  }
+
+  top.innerHTML = symbol;
+  cardNumber.innerHTML = number;
+  bottom.innerHTML = symbol;
 };
